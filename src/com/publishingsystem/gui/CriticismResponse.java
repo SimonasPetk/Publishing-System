6e228532f66174c704e226f0decb8a06294a7c13
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-11-27 13:26:21 +0000

    Respond to criticism, scrollable Q and A

--- src/com/publishingsystem/gui/CriticismResponse.java
+++ src/com/publishingsystem/gui/CriticismResponse.java
@@ -11,9 +11,13 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 import javax.swing.SwingConstants;
 import javax.swing.JTextArea;
@@ -28,10 +32,16 @@ import javax.swing.AbstractListModel;
 import javax.swing.DefaultListModel;
 import javax.swing.ListSelectionModel;
 import javax.swing.JScrollBar;
+import com.publishingsystem.mainclasses.Criticism;
+import com.publishingsystem.mainclasses.Review;
+
+import java.awt.FlowLayout;
 
 public class CriticismResponse {
 
 	private JFrame frmRespondToCriticism;
+	private ArrayList<JTextArea> textAreaAnswers;
+	private ArrayList<Criticism> criticisms;
 
 	/**
 	 * Launch the application.
@@ -40,7 +50,7 @@ public class CriticismResponse {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					CriticismResponse window = new CriticismResponse();
+					CriticismResponse window = new CriticismResponse(new Review(null, null, "Good", "No errors", new ArrayList<Criticism>(), null));
 					window.frmRespondToCriticism.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -52,39 +62,43 @@ public class CriticismResponse {
 	/**
 	 * Create the application.
 	 */
-	public CriticismResponse() {
-		initialize();
+	public CriticismResponse(Review review) {
+		textAreaAnswers= new ArrayList<JTextArea>();
+		criticisms = review.getCriticisms();
+		initialize(review);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize(Review review) {
 		frmRespondToCriticism = new JFrame();
 		frmRespondToCriticism.setTitle("Respond to Criticism");
 		frmRespondToCriticism.setBounds(100, 100, 500, 600);
 		frmRespondToCriticism.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frmRespondToCriticism.setVisible(true);
 		
-		
-		JScrollPane scrPaneAnswer1 = new JScrollPane();
-		scrPaneAnswer1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		
 		JLabel lblYourArticleReviews = new JLabel("Your Article's Reviews");
 		lblYourArticleReviews.setHorizontalAlignment(SwingConstants.CENTER);
 		lblYourArticleReviews.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
-		JScrollPane scrPaneReview1 = new JScrollPane();
-		scrPaneReview1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrPaneReview1.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		
 		JLabel lblReview = new JLabel("Summary");
 		lblReview.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
-		JScrollPane scrPaneAnswer2 = new JScrollPane();
-		scrPaneAnswer2.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		
 		JButton btnSubmitResponse = new JButton("Submit Response");
+		btnSubmitResponse.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				boolean allAnswered = true;
+				for(JTextArea jta : textAreaAnswers) {
+					System.out.println(jta.getText());
+					if(jta.getText().isEmpty())
+						allAnswered = false;
+				}
+				if(!allAnswered)
+					JOptionPane.showMessageDialog(null, "Please answer all criticisms", "Error in response", 0);
+			}
+		});
 		btnSubmitResponse.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		JLabel lblTypographicalErr = new JLabel("Typographical Error");
@@ -97,48 +111,52 @@ public class CriticismResponse {
 		btnAddUpdatedPdf.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		
 		JLabel lblPleaseSubmitYour = new JLabel("Please submit your article's accordingly updated PDF file");
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		
+		JScrollPane scrollPane = new JScrollPane();
 		GroupLayout groupLayout = new GroupLayout(frmRespondToCriticism.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(136)
+					.addComponent(btnSubmitResponse, GroupLayout.PREFERRED_SIZE, 200, Short.MAX_VALUE)
+					.addGap(164))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnAddUpdatedPdf)
+					.addContainerGap(329, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblPleaseSubmitYour, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
+					.addContainerGap())
+				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(150)
-					.addComponent(lblYourArticleReviews, GroupLayout.DEFAULT_SIZE, 184, Short.MAX_VALUE)
+					.addComponent(lblYourArticleReviews, GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
 					.addGap(150))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(lblReview)
-					.addContainerGap(411, Short.MAX_VALUE))
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
+						.addComponent(lblReview))
+					.addContainerGap())
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(scrPaneAnswer1, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
+					.addComponent(lblCriticisms)
+					.addContainerGap(432, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(scrollPane_2, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
 					.addContainerGap())
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
 					.addComponent(lblTypographicalErr, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(301, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblCriticisms)
-					.addContainerGap(415, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(136)
-					.addComponent(btnSubmitResponse, GroupLayout.PREFERRED_SIZE, 184, Short.MAX_VALUE)
-					.addGap(164))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(scrPaneAnswer2, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(btnAddUpdatedPdf)
 					.addContainerGap(321, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap()
-					.addComponent(scrPaneReview1, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(groupLayout.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(lblPleaseSubmitYour, GroupLayout.DEFAULT_SIZE, 464, Short.MAX_VALUE)
+					.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 488, Short.MAX_VALUE)
 					.addContainerGap())
 		);
 		groupLayout.setVerticalGroup(
@@ -148,17 +166,17 @@ public class CriticismResponse {
 					.addComponent(lblYourArticleReviews)
 					.addGap(10)
 					.addComponent(lblReview)
-					.addGap(5)
-					.addComponent(scrPaneReview1, GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
-					.addGap(10)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 99, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
 					.addComponent(lblTypographicalErr, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-					.addGap(5)
-					.addComponent(scrPaneAnswer1, GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
-					.addGap(10)
+					.addGap(18)
+					.addComponent(scrollPane_2, GroupLayout.PREFERRED_SIZE, 53, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
 					.addComponent(lblCriticisms)
-					.addGap(5)
-					.addComponent(scrPaneAnswer2, GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
-					.addGap(10)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 142, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
 					.addComponent(btnAddUpdatedPdf)
 					.addGap(5)
 					.addComponent(lblPleaseSubmitYour)
@@ -167,155 +185,53 @@ public class CriticismResponse {
 					.addGap(10))
 		);
 		
+		JTextArea textAreaSummary = new JTextArea(review.getSummary());
+		textAreaSummary.setEditable(false);
+		scrollPane_2.setViewportView(textAreaSummary);
 		
+		JTextArea textAreaTypeErrors = new JTextArea(review.getTypingErrors());
+		textAreaTypeErrors.setEditable(false);
+		scrollPane_1.setViewportView(textAreaTypeErrors);
 		
-		// ADDIDING ELEMNTS INTO A LIST WILL NEED TO BE OBJECTS of criticsms
-		DefaultListModel listModel = new DefaultListModel();
-		
-		listModel.addElement("smtth");
-		listModel.addElement("smt1");
-		listModel.addElement("smt2");
-		listModel.addElement("smt3");
-		listModel.addElement("smt5");
-		
-		JList list_1 = new JList(listModel);
-		list_1.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list_1.setValueIsAdjusting(true);
-
-		list_1.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				
-				 int index = list_1.getSelectedIndex();
-		         System.out.println("Index Selected: " + index);
-		         String s = (String) list_1.getSelectedValue();
-		         System.out.println("Value Selected: " + s);
-		         
-		         System.out.println(popUp(s));
-		         listModel.remove(index);
-		         listModel.addElement("test");
-		    }
-		});
-		scrPaneAnswer2.setViewportView(list_1);
-		
-		JList list = new JList();
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list.setLayoutOrientation(JList.HORIZONTAL_WRAP);
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"ASSSSSSSSSSSSSSSSSSS", "sdffffffff", "dsfffffffff"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		
-		
-		list.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				
-				/*list.setValueAt("Press Again", 0, 0);
-
-				if (arg0.getClickCount() == 2 && list.rowAtPoint(arg0.getPoint()) == 0) {
-
-			        JOptionPane.showMessageDialog(tblJournal, "row #" + 0 + " is clicked");
-				}*/
-			} 
-		});
-		
-		
-		
-		
-		scrPaneAnswer1.setViewportView(list);
-		
-		JEditorPane dtrpnReview1 = new JEditorPane();
-		dtrpnReview1.setText("\r\n");
-		dtrpnReview1.setEnabled(false);
-		dtrpnReview1.setEditable(false);
-		scrPaneReview1.setViewportView(dtrpnReview1);
-		frmRespondToCriticism.getContentPane().setLayout(groupLayout);
-	}
-	
-	private String popUp(String criticism) {
-		String ans = null;
 		JPanel panel = new JPanel();
+		scrollPane.setViewportView(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
 		
-		
-		JScrollPane scrPaneCriticism = new JScrollPane();
-		scrPaneCriticism.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		
-		JLabel lblAnswerToCriticism = new JLabel("Answer to Criticism");
-		lblAnswerToCriticism.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAnswerToCriticism.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		
-		JLabel lblCriticism = new JLabel("Criticism");
-		lblCriticism.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		
-		JLabel lblYourAnswer = new JLabel("Your Answer");
-		lblYourAnswer.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		
-		JScrollPane scrPaneAnswer = new JScrollPane();
-		scrPaneAnswer.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		
-
-		GroupLayout gl_panel = new GroupLayout(panel);
-		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-							.addGap(250)
-							.addComponent(lblAnswerToCriticism, GroupLayout.DEFAULT_SIZE, 234, Short.MAX_VALUE)
-							.addGap(250))
-						.addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-							.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-								.addComponent(scrPaneAnswer, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 649, Short.MAX_VALUE)
-								.addComponent(lblYourAnswer, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 83, GroupLayout.PREFERRED_SIZE)
-								.addComponent(scrPaneCriticism, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 593, Short.MAX_VALUE)
-								.addComponent(lblCriticism, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE))
-							.addContainerGap())))
-		);
-		gl_panel.setVerticalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(10)
-					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(lblAnswerToCriticism, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-							.addGap(25))
-						.addGroup(gl_panel.createSequentialGroup()
-							.addComponent(lblCriticism, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED)))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(scrPaneCriticism, GroupLayout.DEFAULT_SIZE, 105, Short.MAX_VALUE)
-					.addGap(10)
-					.addComponent(lblYourAnswer, GroupLayout.PREFERRED_SIZE, 19, GroupLayout.PREFERRED_SIZE)
-					.addGap(10)
-					.addComponent(scrPaneAnswer, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE)
-					.addGap(10))
-		);
-		
-		JEditorPane editPaneAnswer = new JEditorPane();
-		scrPaneAnswer.setViewportView(editPaneAnswer);
-		
-		JEditorPane editPaneCriticsm = new JEditorPane();
-		editPaneCriticsm.setText(criticism);
-		editPaneCriticsm.setEditable(false);
-		scrPaneCriticism.setViewportView(editPaneCriticsm);
-		panel.setLayout(gl_panel);
-		
-		int result = JOptionPane.showConfirmDialog(null, panel, "Data Entry", JOptionPane.OK_CANCEL_OPTION);
-		
-		if (result == JOptionPane.OK_OPTION && !editPaneAnswer.getText().isEmpty()) {
-			ans = editPaneAnswer.getText();
-			return ans;
-		} else if (result == JOptionPane.OK_OPTION && editPaneAnswer.getText().isEmpty()) {
-			 JOptionPane.showMessageDialog(new JPanel(), "You haven't responded to criticism", "Warning", JOptionPane.WARNING_MESSAGE);
-		}
-		
-		return null;
+		int qCount = 1;
+		for(int i = 0; i < 4; i++) {
+			JPanel panel_1 = new JPanel();
+			panel_1.setBorder(null);
+			GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+			gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+			gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+			gbc_panel_1.gridx = 0;
+			gbc_panel_1.gridy = i;
+			panel.add(panel_1, gbc_panel_1);
+			panel_1.setLayout(new GridLayout(0, 1, 0, 0));
+			
+			JLabel lblNewLabel = new JLabel("	"+"Q"+qCount+":");
+			lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+			panel_1.add(lblNewLabel);
+			
+			JTextArea textArea = new JTextArea();
+			textArea.setLayout(null);
+			textAreaAnswers.add(textArea);
+			GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+			gbc_textField_1.insets = new Insets(0, 0, 10, 0);
+			gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+			gbc_textField_1.gridx = 0;
+			gbc_textField_1.gridy = i+1;
+			panel.add(textArea, gbc_textField_1);
+			textArea.setColumns(10);
+			textArea.setRows(5);
+			i++;
+			qCount++;
+		}	
+		frmRespondToCriticism.getContentPane().setLayout(groupLayout);
 	}
 }
