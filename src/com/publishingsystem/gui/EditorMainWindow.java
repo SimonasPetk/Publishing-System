3269b73ae60474029e5c186774db15b0b1e327a7
Manas Sarpatwar <mmsarpatwar1@sheffield.ac.uk>
2019-11-27 21:33:17 +0000

    Revert "Editor GUI Journal view to article view commit"
    This reverts commit dc90f7e36b0e2f2a56291fc0e8f7d81bb563c7d2.

--- src/com/publishingsystem/gui/EditorMainWindow.java
+++ src/com/publishingsystem/gui/EditorMainWindow.java
@@ -23,34 +23,21 @@ import javax.swing.table.DefaultTableModel;
 import com.publishingsystem.mainclasses.Academic;
 import com.publishingsystem.mainclasses.Editor;
 import com.publishingsystem.mainclasses.EditorOfJournal;
-import com.publishingsystem.mainclasses.Journal;
-import com.publishingsystem.mainclasses.RetrieveDatabase;
 
 import javax.swing.JLabel;
 import java.awt.Font;
-import java.awt.Point;
 import java.awt.Window;
 
 import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-
-import javax.swing.JPanel;
-import javax.swing.BoxLayout;
-import java.awt.BorderLayout;
-
-import com.publishingsystem.mainclasses.Article;
-import com.publishingsystem.mainclasses.Submission;
-import com.publishingsystem.mainclasses.Review;
 
 public class EditorMainWindow {
 
 	private JFrame frmDashboard;
 	private JTable tblEditor;
 	private Editor editor;
-	private ArrayList<Submission> submissions;
 
 	/**
 	 * Launch the application.
@@ -85,160 +72,170 @@ public class EditorMainWindow {
 		frmDashboard.setBounds(100, 100, 1000, 795);
 		frmDashboard.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmDashboard.setVisible(true);
-
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JLabel lblArticlesList = new JLabel("Article's List:");
+		lblArticlesList.setToolTipText("");
+		lblArticlesList.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
 		JTextArea txtrAbstract = new JTextArea();
 		txtrAbstract.setLineWrap(true);
 		txtrAbstract.setEditable(false);
-
+		
 		JLabel lblAbstract = new JLabel("Abstract:");
 		lblAbstract.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
+		
 		JButton btnPublishArticle = new JButton("Publish Article");
 		btnPublishArticle.setFont(new Font("Tahoma", Font.PLAIN, 15));
-
+		
 		JButton btnRejectArticle = new JButton("Reject Article");
 		btnRejectArticle.setFont(new Font("Tahoma", Font.PLAIN, 15));
-
+		
 		JButton btnDelayPublishing = new JButton("Delay Publishing");
 		btnDelayPublishing.setFont(new Font("Tahoma", Font.PLAIN, 15));
-
-		JPanel panel = new JPanel();
 		GroupLayout groupLayout = new GroupLayout(frmDashboard.getContentPane());
 		groupLayout.setHorizontalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-						.addGap(19)
-						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-								.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(panel, GroupLayout.PREFERRED_SIZE, 596, GroupLayout.PREFERRED_SIZE)
-										.addGap(32)
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-												.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
-												.addComponent(txtrAbstract, GroupLayout.PREFERRED_SIZE, 342, GroupLayout.PREFERRED_SIZE)))
-								.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(btnRejectArticle)
-										.addGap(65)
-										.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
-												.addComponent(btnPublishArticle, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-												.addComponent(btnDelayPublishing, GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE))))
-						.addContainerGap())
-				);
-		groupLayout.setVerticalGroup(
-				groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 604, Short.MAX_VALUE)
+							.addGap(18))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblArticlesList)
+							.addGap(509)))
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(groupLayout.createSequentialGroup()
-										.addGap(10)
-										.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-										.addPreferredGap(ComponentPlacement.RELATED)
-										.addComponent(txtrAbstract, GroupLayout.PREFERRED_SIZE, 369, GroupLayout.PREFERRED_SIZE)
-										.addGap(10)
-										.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-												.addComponent(btnPublishArticle)
-												.addComponent(btnRejectArticle))
-										.addGap(10)
-										.addComponent(btnDelayPublishing))
-								.addGroup(groupLayout.createSequentialGroup()
-										.addContainerGap()
-										.addComponent(panel, GroupLayout.PREFERRED_SIZE, 692, GroupLayout.PREFERRED_SIZE)))
-						.addContainerGap(49, Short.MAX_VALUE))
-				);
-		panel.setLayout(new BorderLayout(0, 0));
-
-		JLabel lblArticlesList = new JLabel("List of Journals:");
-		panel.add(lblArticlesList, BorderLayout.NORTH);
-		lblArticlesList.setToolTipText("");
-		lblArticlesList.setFont(new Font("Tahoma", Font.PLAIN, 20));
-
-		JScrollPane scrollPane = new JScrollPane();
-		panel.add(scrollPane, BorderLayout.CENTER);
-
+							.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
+							.addComponent(txtrAbstract, GroupLayout.PREFERRED_SIZE, 342, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(btnRejectArticle)
+							.addGap(65)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(btnPublishArticle, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(btnDelayPublishing, GroupLayout.DEFAULT_SIZE, 160, Short.MAX_VALUE))))
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(10)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblArticlesList, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(txtrAbstract, GroupLayout.PREFERRED_SIZE, 369, GroupLayout.PREFERRED_SIZE)
+							.addGap(10)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(btnPublishArticle)
+								.addComponent(btnRejectArticle))
+							.addGap(10)
+							.addComponent(btnDelayPublishing)
+							.addContainerGap())
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 673, Short.MAX_VALUE)
+							.addGap(20))))
+		);
+		
 		tblEditor = new JTable();
 		tblEditor.setEnabled(false);
-		DefaultTableModel model = new DefaultTableModel();
-		tblEditor.setModel(model);
-		scrollPane.setViewportView(tblEditor);
-		model.addColumn("ISSN");
-		model.addColumn("Journal Name");
-		model.addColumn("Date of publication");
-		model.addColumn("Chief Editor");
-		model.addColumn("Temporarily retired");
-		for(EditorOfJournal eoj : editor.getEditorOfJournals()) {
-			Journal journal = eoj.getJournal();
-			String[] journalRow = {String.valueOf(journal.getISSN()), 
-					journal.getJournalName(),
-					journal.getDateOfPublication().toString(),
-					eoj.isChiefEditor() ? "yes" : "no",
-							eoj.isTempRetired() ? "yes" : "no"};
-			model.addRow(journalRow);
-		}
 		tblEditor.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				int selectedJournal = Integer.valueOf(String.valueOf(tblEditor.getValueAt(tblEditor.rowAtPoint(arg0.getPoint()), 0)));	
-				boolean isRetired = false;
-				for(EditorOfJournal eoj : editor.getEditorOfJournals()) {
-					if(eoj.isTempRetired() && eoj.isChiefEditor()) {
-						//RAISE SOME ERROR AS TO APPOINT A NEW EDITOR
-					}else if(eoj.isTempRetired()) {
-						JOptionPane.showMessageDialog(null, "You have been retired from this journal due to affiliation conflicts", "Error in showing articles", 0);
-					}
+				
+				tblEditor.setValueAt("Press Again", 0, 0);
+
+				if (arg0.getClickCount() == 2 && tblEditor.rowAtPoint(arg0.getPoint()) == 0) {
+
+			        txtrAbstract.append("This is the abstract of the article you pressed on");
 				}
-				if(!isRetired) {
-					lblArticlesList.setText("List of articles");
-					DefaultTableModel model = (DefaultTableModel) tblEditor.getModel();
-					model.setRowCount(0);
-					model.setColumnCount(0);
-					model.addColumn("Article ID");
-					model.addColumn("Title");
-					model.addColumn("Submission Status");
-					model.addColumn("Reviews revieved");
-					model.addColumn("Verdict 1");
-					model.addColumn("Verdict 2");
-					model.addColumn("Verdict 3");
-					submissions = RetrieveDatabase.getSubmissionsToJournal(selectedJournal);
-					for(Submission s : submissions) {
-						String[] submissionString = new String[7];
-						Article article = s.getArticle();
-						ArrayList<Review> reviews = s.getReviews();
-						submissionString[0] = String.valueOf(article.getArticleId());
-						submissionString[1] = article.getTitle();
-						submissionString[2] = s.getStatus().toString();
-						submissionString[3] = String.valueOf(reviews.size());
-						submissionString[4] = reviews.size() > 0 ? reviews.get(0).getVerdict().toString() : "No verdict";
-						submissionString[5] = reviews.size() > 1 ? reviews.get(1).getVerdict().toString() : "No verdict";
-						submissionString[6] = reviews.size() > 2 ? reviews.get(2).getVerdict().toString() : "No verdict";
-						model.addRow(submissionString);
-						
-					}
-					tblEditor.removeMouseListener(this);
-				}
-			} 
+			}
 		});
-
+		tblEditor.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+				{null, null, null, null, null},
+			},
+			new String[] {
+				"Articles", "Authors", "Date", "Journal", "Status"
+			}
+		));
+		scrollPane.setViewportView(tblEditor);
 		frmDashboard.getContentPane().setLayout(groupLayout);
-
+		
 		JMenuBar menuBar = new JMenuBar();
 		frmDashboard.setJMenuBar(menuBar);
-
+		
 		JMenu mnEditorsMenu = new JMenu("Menu");
 		menuBar.add(mnEditorsMenu);
-
+		
 		JMenuItem mntmRetireFromEditors = new JMenuItem("Retire From Editors");
 		mnEditorsMenu.add(mntmRetireFromEditors);
-
+		
 		JMenuItem mntmChangePassword = new JMenuItem("Change Password");
 		mntmChangePassword.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent arg0) {
 				System.out.print(0);
 				new ChangePassword();
-
+				
 			}
 		});
 
 		mnEditorsMenu.add(mntmChangePassword);
-
+		
 		JMenuItem mntmLogOut = new JMenuItem("Log Out");
 		mntmLogOut.addMouseListener(new MouseAdapter() {
 			@Override
@@ -249,10 +246,10 @@ public class EditorMainWindow {
 			}
 		});
 		mnEditorsMenu.add(mntmLogOut);
-
+		
 		JMenu mnChangeRole = new JMenu("Change My Role");
 		menuBar.add(mnChangeRole);
-
+		
 		if(roles[0] != null) {
 			Editor chiefEditor = (Editor)roles[0];
 			boolean isChiefEditor = false;
@@ -274,7 +271,7 @@ public class EditorMainWindow {
 				mnChangeRole.add(mntmToEditor);
 			}
 		}
-
+		
 		if(roles[1] != null) {
 			JMenuItem mntmToEditor = new JMenuItem("Author");
 			mntmToEditor.addMouseListener(new MouseAdapter() {
@@ -286,20 +283,20 @@ public class EditorMainWindow {
 			});
 			mnChangeRole.add(mntmToEditor);
 		}
-
+		
 		if(roles[2] != null) {
 			JMenuItem mntmToReviewer = new JMenuItem("Reviewer");
 			mntmToReviewer.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mousePressed(MouseEvent e) {
-
+					
 					new ReviewerMainWindow(roles);
 					frmDashboard.dispose();
 				}
 			});
 			mnChangeRole.add(mntmToReviewer);
 		}
-
+		
 		JMenuItem mntmToReader = new JMenuItem("Reader");
 		mntmToReader.addMouseListener(new MouseAdapter() {
 			@Override
