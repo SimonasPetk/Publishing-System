d351ded88ed4ccf2b8b21c89397f9cc7d353a959
Simonas Petkevičius <simonas.petk@gmail.com>
2019-12-04 16:44:48 +0000

    fixed ArticleWindow

--- src/com/publishingsystem/gui/ArticlesWindow.java
+++ src/com/publishingsystem/gui/ArticlesWindow.java
@@ -46,6 +46,12 @@ import java.util.ArrayList;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
 
 public class ArticlesWindow {
 
@@ -88,7 +94,7 @@ public class ArticlesWindow {
 
 		frmAvailableJournalArticles = new JFrame();
 		frmAvailableJournalArticles.setTitle("View Articles");
-		frmAvailableJournalArticles.setBounds(100, 100, width, height);
+		frmAvailableJournalArticles.setBounds(100, 100, 1200, 766);
 		frmAvailableJournalArticles.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmAvailableJournalArticles.setVisible(true);
 
@@ -100,10 +106,6 @@ public class ArticlesWindow {
 
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		JEditorPane editorPane = new JEditorPane();
-		editorPane.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		editorPane.setEditable(false);
-		scrollPane_1.setViewportView(editorPane);
 
 		tblArticles = new JTable();
 		ArrayList<PublishedArticle> allArticles = RetrieveDatabase.getArticles(selJournal.getISSN());
@@ -132,37 +134,6 @@ public class ArticlesWindow {
 		tblArticles.getTableHeader().setFont(new Font("Tahoma", Font.PLAIN, 16));
 		tblArticles.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		scrollPane.setViewportView(tblArticles);
-
-		tblArticles.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent e) {
-
-				int rowPressed = tblArticles.rowAtPoint(e.getPoint());
-				String authors = "";
-				String mainAuthor = "";
-				int articleID = allArticles.get(rowPressed).getArticleId();
-				ArrayList<AuthorOfArticle> authorsList = RetrieveDatabase.getAuthors(articleID);
-				
-				for (AuthorOfArticle author : authorsList) {
-					
-					if (author.isMainAuthor()) {
-						
-						mainAuthor += "Main Author: " + author.getAuthor().getForename() 
-									  + " email: " + author.getAuthor().getEmailId();
-					} else {
-					
-						authors += " " + author.getAuthor().getForename();
-					}					
-				}
-
-				editorPane.setText( allArticles.get(rowPressed).getTitle() + "\n\n" 
-									+ mainAuthor + "\n"
-									+ authors + "\n\n" 
-									+ allArticles.get(rowPressed).getSummary());
-			
-				articleIndexSelected = rowPressed;
-			}
-		});
 
 		JLabel lblJournalName = new JLabel(selJournal.getJournalName());
 		lblJournalName.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -206,35 +177,142 @@ public class ArticlesWindow {
 		});
 
 		GroupLayout groupLayout = new GroupLayout(frmAvailableJournalArticles.getContentPane());
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING).addGroup(groupLayout
-				.createSequentialGroup().addGap(10)
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(10)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addComponent(lblJournalName, GroupLayout.PREFERRED_SIZE, 600, GroupLayout.PREFERRED_SIZE)
-						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE))
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 680, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-								.addPreferredGap(ComponentPlacement.RELATED, 279, Short.MAX_VALUE)
-								.addComponent(btnDownloadPdf).addContainerGap())
-						.addGroup(groupLayout.createSequentialGroup().addPreferredGap(ComponentPlacement.UNRELATED)
-								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 85,
-												GroupLayout.PREFERRED_SIZE)
-										.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 364, Short.MAX_VALUE))
-								.addGap(16)))));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGroup(groupLayout
-				.createSequentialGroup().addGap(10)
-				.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+							.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 468, Short.MAX_VALUE)
+							.addContainerGap())
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(btnDownloadPdf)
+							.addContainerGap())
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+							.addContainerGap(399, Short.MAX_VALUE))))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGap(10)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(lblJournalName, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
 						.addComponent(lblAbstract, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
-				.addGap(10)
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 374, Short.MAX_VALUE).addGap(10)
-								.addComponent(btnDownloadPdf).addGap(165))
-						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE))
-				.addGap(32)));
+					.addGap(10)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+							.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 444, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addComponent(btnDownloadPdf)
+							.addGap(165))
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 630, Short.MAX_VALUE))
+					.addGap(32))
+		);
+		
+		JPanel panel = new JPanel();
+		scrollPane_1.setViewportView(panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblTitle = new JLabel("Title");
+		lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		GridBagConstraints gbc_lblTitle = new GridBagConstraints();
+		gbc_lblTitle.anchor = GridBagConstraints.WEST;
+		gbc_lblTitle.insets = new Insets(0, 0, 5, 0);
+		gbc_lblTitle.gridx = 0;
+		gbc_lblTitle.gridy = 0;
+		panel.add(lblTitle, gbc_lblTitle);
+		
+		JEditorPane titleArea = new JEditorPane();
+		titleArea.setEditable(false);
+		titleArea.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GridBagConstraints gbc_titleArea = new GridBagConstraints();
+		gbc_titleArea.insets = new Insets(0, 0, 5, 0);
+		gbc_titleArea.fill = GridBagConstraints.BOTH;
+		gbc_titleArea.gridx = 0;
+		gbc_titleArea.gridy = 1;
+		panel.add(titleArea, gbc_titleArea);
+		
+		JLabel lblAuthors = new JLabel("Authors");
+		lblAuthors.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		GridBagConstraints gbc_lblAuthors = new GridBagConstraints();
+		gbc_lblAuthors.anchor = GridBagConstraints.WEST;
+		gbc_lblAuthors.insets = new Insets(0, 0, 5, 0);
+		gbc_lblAuthors.gridx = 0;
+		gbc_lblAuthors.gridy = 2;
+		panel.add(lblAuthors, gbc_lblAuthors);
+		
+		JEditorPane authorArea = new JEditorPane();
+		authorArea.setEditable(false);
+		authorArea.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GridBagConstraints gbc_authorArea = new GridBagConstraints();
+		gbc_authorArea.insets = new Insets(0, 0, 5, 0);
+		gbc_authorArea.fill = GridBagConstraints.BOTH;
+		gbc_authorArea.gridx = 0;
+		gbc_authorArea.gridy = 3;
+		panel.add(authorArea, gbc_authorArea);
+		
+		JLabel lblSummary = new JLabel("Abstract");
+		lblSummary.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		GridBagConstraints gbc_lblSummary = new GridBagConstraints();
+		gbc_lblSummary.anchor = GridBagConstraints.WEST;
+		gbc_lblSummary.insets = new Insets(0, 0, 5, 0);
+		gbc_lblSummary.gridx = 0;
+		gbc_lblSummary.gridy = 4;
+		panel.add(lblSummary, gbc_lblSummary);
+		
+		JEditorPane abstractArea = new JEditorPane();
+		abstractArea.setEditable(false);
+		abstractArea.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		GridBagConstraints gbc_abstractArea = new GridBagConstraints();
+		gbc_abstractArea.fill = GridBagConstraints.BOTH;
+		gbc_abstractArea.gridx = 0;
+		gbc_abstractArea.gridy = 5;
+		panel.add(abstractArea, gbc_abstractArea);
 
 		frmAvailableJournalArticles.getContentPane().setLayout(groupLayout);
+		
+		tblArticles.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+
+				int rowPressed = tblArticles.rowAtPoint(e.getPoint());
+				String authors = "";
+				String mainAuthor = "";
+				int articleID = allArticles.get(rowPressed).getArticleId();
+				ArrayList<AuthorOfArticle> authorsList = RetrieveDatabase.getAuthors(articleID);
+				String articleInfo;
+				
+				for (AuthorOfArticle author : authorsList) {
+					if (author.isMainAuthor()) {
+						
+						mainAuthor += author.getAuthor().getForename() 
+									  + " email: " + author.getAuthor().getEmailId();
+					} else {
+					
+						authors += author.getAuthor().getForename() + " ";
+					}					
+				}
+
+				titleArea.setText(allArticles.get(rowPressed).getTitle());
+
+				authorArea.setText(mainAuthor + "\n" + authors);
+				abstractArea.setText(allArticles.get(rowPressed).getSummary());
+				
+				articleIndexSelected = rowPressed;
+			}
+		});
+		
 
 		JMenuBar menuBar = new JMenuBar();
 		frmAvailableJournalArticles.setJMenuBar(menuBar);
